105905cce81c5c42f1ee359ee1818ca4a6538a68
Coding Pirate <chintanpanchal396@gmail.com>
2025-03-04 20:29:29 -0500

    added some generic data

--- Nudge App/NudgeApp/Manager/NotificationManager.swift
+++ Nudge App/NudgeApp/Manager/NotificationManager.swift
@@ -10,10 +10,31 @@ import UserNotifications
 class NotificationManager {
     
     static var shared = NotificationManager()
-    private var arrNotificationQuotes = [""]
-    
-    private var arrCharacters : [String] = [""]
-    
+    private var arrNotificationQuotes = [
+        "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        "Believe you can and you're halfway there.",
+        "The only limit to our realization of tomorrow is our doubts of today.",
+        "Don't watch the clock; do what it does. Keep going.",
+        "The future depends on what you do today.",
+        "It always seems impossible until it's done.",
+        "Opportunities don't happen. You create them.",
+        "Dream big and dare to fail.",
+        "Hardships often prepare ordinary people for an extraordinary destiny.",
+        "Do what you can, with what you have, where you are."
+    ]
+        
+    let motivationalWords = [
+        "Believe",
+        "Achieve",
+        "Persist",
+        "Focus",
+        "Courage",
+        "Inspire",
+        "Grow",
+        "Thrive",
+        "Create",
+        "Empower"
+    ]
     
     private let center = UNUserNotificationCenter.current()
     
@@ -31,8 +52,8 @@ class NotificationManager {
                 
                 if let notificationDate = calendar.date(from: components) {
                     let quote = self.arrNotificationQuotes.randomElement() ?? "Are you following your routine?"
-                    let character = self.arrCharacters.randomElement() ?? "Chintan"
-                    self.scheduleNotification(at: notificationDate, title: "Hey \(character)!", body: quote)
+                    let word = self.motivationalWords.randomElement() ?? "Believe"
+                    self.scheduleNotification(at: notificationDate, title: "\(word)!", body: quote)
                 }
             }
         }
